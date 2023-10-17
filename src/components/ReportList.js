a7f6c11dfb890d63348dbf4ee8f94a8ea1225ba2
Vidoje Radosavljevic <vidoje.radosavljevic@rs.ey.com>
2023-10-17 12:58:27 +0200

    add timestamp

--- src/components/ReportList.js
+++ src/components/ReportList.js
@@ -3,9 +3,12 @@ import classes from './ReportList.module.scss';
 import ReportItem from './ReportItem';
 
 function ReportList({ reportData, category }) {
+  const date = new Date(reportData.timestamp);
+
   return (
     category !== '' && (
       <div className={classes['report-list']}>
+        <p>Timestamp: {` ${date}`}</p>
         <div className={classes.headline}>
           <h2>{category.toUpperCase()}</h2>
         </div>
