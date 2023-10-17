c858af98c8c1c314bf0f16c5a4950bc9d65fde8f
Vidoje Radosavljevic <vidoje.radosavljevic@rs.ey.com>
2023-10-17 13:55:32 +0200

    fix Violations and Inapplicable

--- src/components/ReportItem.js
+++ src/components/ReportItem.js
@@ -20,137 +20,244 @@ function ReportItem({ reportData, category }) {
                     start={nodeIndex + 1}
                     key={Math.random()}
                   >
-                    {node.any.map((el) => (
-                      <li key={Math.random()}>
-                        <div className={classes.node}>
-                          <div className={classes.message}>
-                            <h4>{el.message}</h4>
-                          </div>
-                          <div className={classes.html}>
-                            <span
-                              style={{
-                                display: 'block',
-                                fontWeight: 'bold',
-                                marginLeft: 1 + 'rem',
-                              }}
-                            >
-                              html:
-                            </span>
-                            <p>{node.html}</p>
-                          </div>
-                          <div className={classes.html}>
-                            <span
-                              style={{
-                                display: 'block',
-                                fontWeight: 'bold',
-                                marginLeft: 1 + 'rem',
-                              }}
-                            >
-                              id:
-                            </span>
-                            <p>{item.id}</p>
-                          </div>
-                          {node.impact && (
-                            <div className={classes.impact}>
-                              <span
-                                style={{
-                                  paddingRight: 1 + 'rem',
-                                  fontWeight: 'bold',
-                                  marginLeft: 1 + 'rem',
-                                }}
-                              >
-                                impact:
-                              </span>
-                              <p>{node.impact}</p>
+                    {node.any.length > 0
+                      ? node.any.map((el) => (
+                          <li key={Math.random()}>
+                            <div className={classes.node}>
+                              <div className={classes.message}>
+                                <h4>{el.message}</h4>
+                              </div>
+                              <div className={classes.html}>
+                                <span
+                                  style={{
+                                    display: 'block',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  html:
+                                </span>
+                                <p>{node.html}</p>
+                              </div>
+                              <div className={classes.html}>
+                                <span
+                                  style={{
+                                    display: 'block',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  id:
+                                </span>
+                                <p>{item.id}</p>
+                              </div>
+                              {node.impact && (
+                                <div className={classes.impact}>
+                                  <span
+                                    style={{
+                                      paddingRight: 1 + 'rem',
+                                      fontWeight: 'bold',
+                                      marginLeft: 1 + 'rem',
+                                    }}
+                                  >
+                                    impact:
+                                  </span>
+                                  <p>{node.impact}</p>
+                                </div>
+                              )}
+                              <div className={classes.target}>
+                                <span
+                                  style={{
+                                    paddingRight: 1 + 'rem',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  target:
+                                </span>
+                                <p>{node.target}</p>
+                              </div>
+                              <div className={classes.target}>
+                                <span
+                                  style={{
+                                    paddingRight: 1 + 'rem',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  suggestion:
+                                </span>
+                                <a href={item.helpUrl}>{item.helpUrl}</a>
+                              </div>
+                              <div className={classes.target}>
+                                <span
+                                  style={{
+                                    display: 'block',
+                                    paddingRight: 1 + 'rem',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  tags:
+                                </span>
+                                {item.tags.map((tag) => (
+                                  <p
+                                    key={Math.random()}
+                                    style={{ display: 'inline-block' }}
+                                  >
+                                    {tag}
+                                  </p>
+                                ))}
+                              </div>
                             </div>
-                          )}
-                          <div className={classes.target}>
-                            <span
-                              style={{
-                                paddingRight: 1 + 'rem',
-                                fontWeight: 'bold',
-                                marginLeft: 1 + 'rem',
-                              }}
-                            >
-                              target:
-                            </span>
-                            <p>{node.target}</p>
-                          </div>
-                          <div className={classes.target}>
-                            <span
-                              style={{
-                                paddingRight: 1 + 'rem',
-                                fontWeight: 'bold',
-                                marginLeft: 1 + 'rem',
-                              }}
-                            >
-                              suggestion:
-                            </span>
-                            <a href={item.helpUrl}>{item.helpUrl}</a>
-                          </div>
-                          <div className={classes.target}>
-                            <span
-                              style={{
-                                display: 'block',
-                                paddingRight: 1 + 'rem',
-                                fontWeight: 'bold',
-                                marginLeft: 1 + 'rem',
-                              }}
-                            >
-                              tags:
-                            </span>
-                            {item.tags.map((tag) => (
-                              <p style={{ display: 'inline-block' }}>{tag}</p>
-                            ))}
-                          </div>
-                        </div>
-                      </li>
-                    ))}
+                          </li>
+                        ))
+                      : node.all.map((el) => (
+                          <li key={Math.random()}>
+                            <div className={classes.node}>
+                              <div className={classes.message}>
+                                <h4>{el.message}</h4>
+                              </div>
+                              <div className={classes.html}>
+                                <span
+                                  style={{
+                                    display: 'block',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  html:
+                                </span>
+                                <p>{node.html}</p>
+                              </div>
+                              <div className={classes.html}>
+                                <span
+                                  style={{
+                                    display: 'block',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  id:
+                                </span>
+                                <p>{item.id}</p>
+                              </div>
+                              {node.impact && (
+                                <div className={classes.impact}>
+                                  <span
+                                    style={{
+                                      paddingRight: 1 + 'rem',
+                                      fontWeight: 'bold',
+                                      marginLeft: 1 + 'rem',
+                                    }}
+                                  >
+                                    impact:
+                                  </span>
+                                  <p>{node.impact}</p>
+                                </div>
+                              )}
+                              <div className={classes.target}>
+                                <span
+                                  style={{
+                                    paddingRight: 1 + 'rem',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  target:
+                                </span>
+                                <p>{node.target}</p>
+                              </div>
+                              <div className={classes.target}>
+                                <span
+                                  style={{
+                                    paddingRight: 1 + 'rem',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  suggestion:
+                                </span>
+                                <a href={item.helpUrl}>{item.helpUrl}</a>
+                              </div>
+                              <div className={classes.target}>
+                                <span
+                                  style={{
+                                    display: 'block',
+                                    paddingRight: 1 + 'rem',
+                                    fontWeight: 'bold',
+                                    marginLeft: 1 + 'rem',
+                                  }}
+                                >
+                                  tags:
+                                </span>
+                                {item.tags.map((tag) => (
+                                  <p
+                                    key={Math.random()}
+                                    style={{ display: 'inline-block' }}
+                                  >
+                                    {tag}
+                                  </p>
+                                ))}
+                              </div>
+                            </div>
+                          </li>
+                        ))}
                   </ol>
                 ))}
               </div>
             ) : (
               <div className={classes.inapplicable}>
-                <span style={{ paddingRight: 0.5 + 'rem' }}></span>{' '}
-                <div className={classes.html}>
-                  <span
-                    style={{
-                      display: 'block',
-                      fontWeight: 'bold',
-                      marginLeft: 1 + 'rem',
-                    }}
-                  >
-                    id:
-                  </span>
-                  <p>{item.id}</p>
-                </div>
-                <div className={classes.target}>
-                  <span
-                    style={{
-                      paddingRight: 1 + 'rem',
-                      fontWeight: 'bold',
-                      marginLeft: 1 + 'rem',
-                    }}
-                  >
-                    suggestion:
-                  </span>
-                  <a href={item.helpUrl}>{item.helpUrl}</a>
-                </div>
-                <div className={classes.target}>
-                  <span
-                    style={{
-                      display: 'block',
-                      paddingRight: 1 + 'rem',
-                      fontWeight: 'bold',
-                      marginLeft: 1 + 'rem',
-                    }}
-                  >
-                    tags:
-                  </span>
-                  {item.tags.map((tag) => (
-                    <p style={{ display: 'inline-block' }}>{tag}</p>
-                  ))}
-                </div>
+                <ol
+                  start={1}
+                  key={Math.random()}
+                >
+                  <span style={{ paddingRight: 0.5 + 'rem' }}></span>{' '}
+                  <div className={classes.html}>
+                    <span
+                      style={{
+                        display: 'block',
+                        fontWeight: 'bold',
+                        marginLeft: 1 + 'rem',
+                      }}
+                    >
+                      id:
+                    </span>
+                    <p>{item.id}</p>
+                  </div>
+                  <div className={classes.target}>
+                    <span
+                      style={{
+                        paddingRight: 1 + 'rem',
+                        fontWeight: 'bold',
+                        marginLeft: 1 + 'rem',
+                      }}
+                    >
+                      suggestion:
+                    </span>
+                    <a href={item.helpUrl}>{item.helpUrl}</a>
+                  </div>
+                  <div className={classes.target}>
+                    <span
+                      style={{
+                        display: 'block',
+                        paddingRight: 1 + 'rem',
+                        fontWeight: 'bold',
+                        marginLeft: 1 + 'rem',
+                      }}
+                    >
+                      tags:
+                    </span>
+                    {item.tags.map((tag) => (
+                      <p
+                        key={Math.random()}
+                        style={{ display: 'inline-block' }}
+                      >
+                        {tag}
+                      </p>
+                    ))}
+                  </div>
+                </ol>
               </div>
             )}
           </div>
